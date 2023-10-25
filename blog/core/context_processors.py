f55a002330ad54617094cd294058d1ecdf49a126
Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>
2023-10-25 12:10:31 +0100

    Delete blog directory

--- blog/core/context_processors.py
+++
@@ -1,5 +0,0 @@
-from .models import Category
-
-def categories(request):
-    # Adjust your filtering logic based on an existing field (e.g., 'name')
-    return {"categories": Category.objects.filter(name='name')}
--- blog/static/core/javascript/script.js
+++
@@ -1,9 +0,0 @@
-function toggleDropdown(elementId) {
-    var dropdownContent = document.getElementById(elementId);
-
-    if (dropdownContent.style.display === 'none') {
-        dropdownContent.style.display = 'flex'; // Change display to 'flex' to show the dropdown
-    } else {
-        dropdownContent.style.display = 'none';
-    }
-}
